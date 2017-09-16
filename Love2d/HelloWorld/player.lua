bac1d4be54219b148223c44b8e0343c57ece48d0
Alan Tyloo <adngtcodin@live.com>
2017-09-16 22:52:16 +0400

    plane sound

--- Love2d/HelloWorld/player.lua
+++ Love2d/HelloWorld/player.lua
@@ -1,7 +1,8 @@
-local Player = {
-    images = {},
-    x = 0,
-    y = 0,
+local Player =  {
+    images =  {}, 
+    planeSound = nil, 
+    x = 0, 
+    y = 0, 
     animation = 'normal'
 }
 
@@ -16,33 +17,43 @@ end
 
 function Player:load()
     self:loadImages()
+    self:loadSounds()
 end
 
 function Player:loadImages()
-    self.images = {}
-    self.images["normal"] = love.graphics.newImage('assets/plane/plane_0_titi_son.png')
-    self.images["tilt-right"] = love.graphics.newImage('assets/plane/plane_1_titi_son.png')
-    self.images["tilt-left"] = love.graphics.newImage('assets/plane/plane_2_titi_son.png')
+    self.images =  {}
+    self.images["normal"] = love.graphics.newImage('assets/images/plane/plane_0_titi_son.png')
+    self.images["tilt-right"] = love.graphics.newImage('assets/images/plane/plane_1_titi_son.png')
+    self.images["tilt-left"] = love.graphics.newImage('assets/images/plane/plane_2_titi_son.png')
+end
+
+function Player:loadSounds()
+    self.planeSound = love.audio.newSource("assets/sounds/plane.ogg", "static")
+    self.planeSound:setLooping(true)
+    self.planeSound:play()
 end
 
 function Player:update(dt)
     self.animation = 'normal'
-    
-    if love.keyboard.isDown("left") then
+    self.planeSound:setPitch(1)
+
+    if love.keyboard.isDown("left")then
         self.animation = 'tilt-left'
         self.x = self.x - (moveSpeed * dt)
+        self.planeSound:setPitch(1.5)
     end
 
-    if love.keyboard.isDown("right") then
+    if love.keyboard.isDown("right")then
         self.animation = 'tilt-right'
         self.x = self.x + (moveSpeed * dt)
+        self.planeSound:setPitch(1.5)
     end
 
-    if love.keyboard.isDown("up") then
+    if love.keyboard.isDown("up")then
         self.y = self.y - (moveSpeed * dt)
     end
 
-    if love.keyboard.isDown("down") then
+    if love.keyboard.isDown("down")then
         self.y = self.y + (moveSpeed * dt)
     end
 end
